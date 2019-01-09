cd0a52e6eeaae8190ca92ed2b810cca1ab02689a
Gyan Gupta <gyan.gupta@zigway.co>
2019-01-09 18:18:51 +0530

    first commit

---
+++ pdf_generater/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class PdfGeneraterConfig(AppConfig):
+    name = 'pdf_generater'
---
+++ pdf_generater/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'upload.html')
