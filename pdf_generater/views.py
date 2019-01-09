3593cd77517d8c057ae141e2101e53e376140eff
Gyan Gupta <gyan.gupta@zigway.co>
2019-01-09 19:42:41 +0530

    modify code

--- pdf_generater/views.py
+++ pdf_generater/views.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import subprocess
+import os
+import uuid
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 def upload(request):
@@ -11,7 +17,27 @@ def upload(request):
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+        
+        html = document_to_html(uploaded_file_url)
         return render(request, 'upload.html', {
-            'uploaded_file_url': uploaded_file_url
+            'uploaded_file_url': uploaded_file_url,
+            'html1':html
         })
     return render(request, 'upload.html')
+
+
+def document_to_html(file_path):
+    tmp = "/tmp"
+    # file_path1 = os.path.join(settings.BASE_DIR, file_path)
+    file_path1 = '/home/rails/Projects/Alok/pdf_reader/pdf_reader'+file_path
+    guid = str(uuid.uuid1())
+    # convert the file, using a temporary file w/ a random name
+    command = "abiword -t %(tmp)s/%(guid)s.html %(file_path1)s; cat %(tmp)s/%(guid)s.html" % locals()
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=os.path.join(settings.BASE_DIR, "templates"))
+    
+    error = p.stderr.readlines()
+    import pdb;pdb.set_trace()
+    if error:
+        raise Exception("".join(error))
+    html = p.stdout.readlines()
+    return "".join(html)
